84dee14d1a7982d166eca3ce46b486c6f65d20e2
sabujghorai <ghoraisabuj4@gmail.com>
2026-08-12 14:31:20 +0530

    implementing the encryption and descrypion code

---
+++ PBL/project3.py
@@ -0,0 +1,21 @@
+def encrypt(text):
+    result = ""
+    for char in text:
+        result += chr(ord(char) + 3)
+    return result
+
+
+def decrypt(text):
+    result = ""
+    for char in text:
+        result += chr(ord(char) - 3)
+    return result
+
+
+text = input("Enter string: ")
+
+encrypted = encrypt(text)
+print("Encrypted Code is :", encrypted)
+
+decrypted = decrypt(encrypted)
+print("Decrypted code is :", decrypted)
