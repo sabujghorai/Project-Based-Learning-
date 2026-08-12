ebb7167f975eab6a62397ef0ad40a3a1712c98c6
sabujghorai <ghoraisabuj4@gmail.com>
2026-08-12 14:48:59 +0530

    updated the project file

--- PBL/project3.py
+++ PBL/project3.py
@@ -1,19 +1,19 @@
 def encrypt(text):
-    result = " "
+    result = ""
     for char in text:
         result += chr(ord(char) + 3)
         return result
 
 def decrypt(text):
-    result = " "
+    result = ""
     for char in text:
-        result += char(ord(char) - 3)
+        result += chr(ord(char) - 3)
         return result
 
-string = int(input("Enter the string :"))
+string = input("Enter the string :")
 
 encrypted = encrypt(string)
 print("Encrypted String is : ",encrypted)
 
 decrypted = decrypt(string)
-print("Decrypted String is :",decrypt)
+print("Decrypted String is :",decrypted)
