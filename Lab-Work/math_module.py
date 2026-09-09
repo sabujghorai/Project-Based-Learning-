d6d0975786240258dbefaa23358aea82c33ee91a
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-09 14:17:07 +0530

    updated the math_module.py file

--- Lab-Work/math_module.py
+++ Lab-Work/math_module.py
@@ -1,4 +1,9 @@
 # WAP in python to find the factorial of a number using math module
 import math
-fact = int(input("Enter a number :"))
-print(f"Factorial is : {math.factorial(fact)}")
+# fact = int(input("Enter a number :"))
+# print(f"Factorial is : {math.factorial(fact)}")
+
+
+# WAP to find the area of a circle using math module
+radius = int(input("Enter the radius :"))
+print(f"Area is : {math.pi*math.pow(radius,2)}")
