c6026f2a47f6be9d2d4da1aead69752236efa92e
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-09 14:25:22 +0530

    updated the math_module.py file

--- Lab-Work/math_module.py
+++ Lab-Work/math_module.py
@@ -1,12 +1,12 @@
 # WAP in python to find the factorial of a number using math module
 import math
-# fact = int(input("Enter a number :"))
-# print(f"Factorial is : {math.factorial(fact)}")
+fact = int(input("Enter a number :"))
+print(f"Factorial is : {math.factorial(fact)}")
 
 
-# # WAP to find the area of a circle using math module
-# radius = int(input("Enter the radius :"))
-# print(f"Area is : {math.pi*math.pow(radius,2)}")
+# WAP to find the area of a circle using math module
+radius = int(input("Enter the radius :"))
+print(f"Area is : {math.pi*math.pow(radius,2)}")
 
 
 # WAP in python program to find the gcd of two numbers using math Module
