28bda66124c9abe797981a80edf40d883de5eede
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-09 14:23:43 +0530

    updated the math_module.py file

--- Lab-Work/math_module.py
+++ Lab-Work/math_module.py
@@ -4,6 +4,20 @@ import math
 # print(f"Factorial is : {math.factorial(fact)}")
 
 
-# WAP to find the area of a circle using math module
-radius = int(input("Enter the radius :"))
-print(f"Area is : {math.pi*math.pow(radius,2)}")
+# # WAP to find the area of a circle using math module
+# radius = int(input("Enter the radius :"))
+# print(f"Area is : {math.pi*math.pow(radius,2)}")
+
+
+# WAP in python program to find the gcd of two numbers using math Module
+
+a = int(input("Enter A :"))
+b = int(input("Enter B :"))
+print(f"The GCD is : {math.gcd(a,b)}")
+
+
+
+# WAP in python program to find the LCM of two numbers using math Module
+a = int(input("Enter A :"))
+b = int(input("Enter B :"))
+print(f"The GCD is : {math.lcm(a,b)}")
