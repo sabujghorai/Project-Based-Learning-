c90be36ad423a9388f5b81b77e1fb9ff566e0453
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-09 14:43:25 +0530

    updated the math_module.py file

--- Lab-Work/math_module.py
+++ Lab-Work/math_module.py
@@ -1,25 +1,25 @@
 # # WAP in python to find the factorial of a number using math module
 import math
-# fact = int(input("Enter a number :"))
-# print(f"Factorial is : {math.factorial(fact)}")
+fact = int(input("Enter a number :"))
+print(f"Factorial is : {math.factorial(fact)}")
 
 
-# # WAP to find the area of a circle using math module
-# radius = int(input("Enter the radius :"))
-# print(f"Area is : {math.pi*math.pow(radius,2)}")
+# WAP to find the area of a circle using math module
+radius = int(input("Enter the radius :"))
+print(f"Area is : {math.pi*math.pow(radius,2)}")
 
 
-# # WAP in python program to find the gcd of two numbers using math Module
+# WAP in python program to find the gcd of two numbers using math Module
 
-# a = int(input("Enter A :"))
-# b = int(input("Enter B :"))
-# print(f"The GCD is : {math.gcd(a,b)}")
+a = int(input("Enter A :"))
+b = int(input("Enter B :"))
+print(f"The GCD is : {math.gcd(a,b)}")
 
 
-# # WAP in python program to find the LCM of two numbers using math Module
-# c = int(input("Enter A :"))
-# d = int(input("Enter B :"))
-# print(f"The GCD is : {math.lcm(c,d)}")
+# WAP in python program to find the LCM of two numbers using math Module
+c = int(input("Enter A :"))
+d = int(input("Enter B :"))
+print(f"The GCD is : {math.lcm(c,d)}")
 
 
 # WAP to calculate the distance between two coordinates 
